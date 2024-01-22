5b17e02d5c94b13aabd470b46c9894665212bd0b
Zujan <10851371-Zujan@users.noreply.replit.com>
2024-01-22 08:16:52 +0000

    Plaatjes eringezet voor sprite; Rakket erin gezet en laten besturen, sprite voor die animatie

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+var shipImage;
+var ship;
+var MARGIN = 40;
+var SHIP_NORMAL = 'normal';
+var SHIP_THRUST = 'thrust';
+
+function preload() {
+  shipImage = loadImage('assets/asteroids_ship0001.png');
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+  createCanvas(1200, 725);
+
+  ship = createSprite(width / 2, height / 2);
+  ship.debug = false;
+  ship.maxSpeed = 6;
+  ship.friction = 0.01;
+  ship.addImage(SHIP_NORMAL, shipImage);
+  ship.addAnimation(SHIP_THRUST, 'assets/asteroids_ship0002.png', 'assets/asteroids_ship0007.png');
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  background(0);
+
+  // Ship controls
+  if (keyDown(LEFT_ARROW)) {
+    ship.rotation -= 4;
+  }
+  if (keyDown(RIGHT_ARROW)) {
+    ship.rotation += 4;
+  }
+  if (keyDown(UP_ARROW)) {
+    ship.addSpeed(0.2, ship.rotation);
+    ship.changeAnimation(SHIP_THRUST);
+  } else {
+    ship.changeAnimation(SHIP_NORMAL);
+  }
+
+  // Wrap around the screen
+  if (ship.position.x < -MARGIN) {
+    ship.position.x = width + MARGIN;
+  }
+  if (ship.position.x > width + MARGIN) {
+    ship.position.x = -MARGIN;
+  }
+  if (ship.position.y < -MARGIN) {
+    ship.position.y = height + MARGIN;
+  }
+  if (ship.position.y > height + MARGIN) {
+    ship.position.y = -MARGIN;
+  }
+
+  drawSprites();
 }
