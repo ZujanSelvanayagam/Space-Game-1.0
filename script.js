086e92ec4bbdc727c88843f8663d4283f3852145
ZujanSelvanayagam <95480992+ZujanSelvanayagam@users.noreply.github.com>
2024-01-22 09:05:03 +0000

    Ook tweede rakketje gefixd; twee ships aangemaakt

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var shipImage;
-var ship;
+var ships = [];
 var MARGIN = 40;
 var SHIP_NORMAL = 'normal';
 var SHIP_THRUST = 'thrust';
@@ -11,43 +11,77 @@ function preload() {
 function setup() {
   createCanvas(1200, 725);
 
-  ship = createSprite(width / 2, height / 2);
-  ship.debug = false;
-  ship.maxSpeed = 6;
-  ship.friction = 0.01;
-  ship.addImage(SHIP_NORMAL, shipImage);
-  ship.addAnimation(SHIP_THRUST, 'assets/asteroids_ship0002.png', 'assets/asteroids_ship0007.png');
+  ships = [];
+
+  for (var i = 0; i < 2; i++) {
+    var newShip = createSprite(width / 2 + i * 50, height / 2);
+    newShip.debug = false;
+    newShip.maxSpeed = 6;
+    newShip.friction = 0.01;
+    newShip.addImage(SHIP_NORMAL, shipImage);
+    newShip.addAnimation(SHIP_THRUST, 'assets/asteroids_ship0002.png', 'assets/asteroids_ship0007.png');
+    ships.push(newShip);
+  }
 }
 
 function draw() {
   background(0);
+  drawUi();
+}
+
+function drawUi() {
+  for (var i = 0; i < allSprites.length; i++) {
+    var sprite = allSprites[i];
+    if (sprite.position.x < -MARGIN) {
+      sprite.position.x = width + MARGIN;
+    }
+    if (sprite.position.x > width + MARGIN) {
+      sprite.position.x = -MARGIN;
+    }
+    if (sprite.position.y < -MARGIN) {
+      sprite.position.y = height + MARGIN;
+    }
+    if (sprite.position.y > height + MARGIN) {
+      sprite.position.y = -MARGIN;
+    }
+  }
 
   // Ship controls
-  if (keyDown(LEFT_ARROW)) {
-    ship.rotation -= 4;
-  }
-  if (keyDown(RIGHT_ARROW)) {
-    ship.rotation += 4;
-  }
-  if (keyDown(UP_ARROW)) {
-    ship.addSpeed(0.2, ship.rotation);
-    ship.changeAnimation(SHIP_THRUST);
-  } else {
-    ship.changeAnimation(SHIP_NORMAL);
-  }
+  for (var i = 0; i < ships.length; i++) {
+    var currentShip = ships[i];
 
-  // Wrap around the screen
-  if (ship.position.x < -MARGIN) {
-    ship.position.x = width + MARGIN;
-  }
-  if (ship.position.x > width + MARGIN) {
-    ship.position.x = -MARGIN;
-  }
-  if (ship.position.y < -MARGIN) {
-    ship.position.y = height + MARGIN;
-  }
-  if (ship.position.y > height + MARGIN) {
-    ship.position.y = -MARGIN;
+    if (i === 0) {
+      // Controls for the first ship
+      if (keyDown(LEFT_ARROW)) {
+        currentShip.rotation -= 4;
+      }
+      if (keyDown(RIGHT_ARROW)) {
+        currentShip.rotation += 4;
+      }
+      if (keyDown(UP_ARROW)) {
+        currentShip.addSpeed(0.2, currentShip.rotation);
+        currentShip.changeAnimation(SHIP_THRUST);
+      } else {
+        currentShip.changeAnimation(SHIP_NORMAL);
+      }
+    }
+
+    if (i === 1) {
+      // Controls for the second ship
+      if (keyDown(65)) {
+        currentShip.rotation -= 4;
+      }
+      if (keyDown(68)) {
+        currentShip.rotation += 4;
+      }
+
+      if (keyDown(87)) {
+        currentShip.addSpeed(0.2, currentShip.rotation);
+        currentShip.changeAnimation(SHIP_THRUST);
+      } else {
+        currentShip.changeAnimation(SHIP_NORMAL);
+      }
+    }
   }
 
   drawSprites();
